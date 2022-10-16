dbedb8c0b71af23d4e0970628b9ec2c0775d51f4
Katie Walsh <kwalsh7642@gmail.com>
2022-10-16 13:05:59 -0500

    refactored version

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,57 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+
+
+    private int id;
+    private static int nextId = 1;
+
+    private String value;
+
+    // getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    //custom methods
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField that = (JobField) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String aValue) {
+        this();
+        this.value = aValue;
+    }
+
+}
